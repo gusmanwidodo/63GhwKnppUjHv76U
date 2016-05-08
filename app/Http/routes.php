9215d1bb4fe79c38ce130e4de698766689559b4d
Gusman Widodo <gusmanwidodo@gmail.com>
2016-05-08 11:31:08 +0800

    company 7&8

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -660,6 +660,197 @@ Route::get('/company6', function () use ($proxies, $proxy) {
 
 });
 
+Route::get('/company7', function () use ($proxies, $proxy) {
+
+    if (isset($proxies)) {
+        $proxy = $proxies[array_rand($proxies)];
+    }
+
+    set_time_limit(1000);   
+
+
+        $i = File::get(storage_path('count7.txt'));
+
+        $i = (int) $i;
+
+        $page = new Dom;
+
+        $page->loadFromUrlProxy('http://m.indonetwork.co.id/search?type=company&page=' . $i, $proxy);
+
+        foreach ($page->find('.list .listingdata') as $list) {
+
+            $link_dom = $list->find('.productdataimg a', 0);
+
+            if ($link_dom) {
+
+            $link = 'http:' . $link_dom->getAttribute('href');
+
+            $thumb = $list->find('.productdataimg a img', 0);
+
+            $logo = 'http://m.indonetwork.co.id' . $thumb->getAttribute('src');
+
+            $name = $list->find('.productdata .content h3 a', 0)->text;
+
+            $address = $list->find('.productdata .companytitle h4', 0)->text;
+
+
+            $company_dom = new Dom;
+            $company_dom->loadFromUrlProxy($link .'/info', $proxy);
+
+            $dict = [
+                'contact_person', 'email', 'phone', 'alamat', 'kodepos', 'negara', 'provinces', 'kota', 'website', 'gabung', 'update'
+            ];
+
+            $j = 0;
+            $comp = [];
+
+            foreach($company_dom->find('.userpage .rightcontent .form .column-9') as $val){
+                if($dict[$j] == 'email' || $dict[$j] == 'phone'){
+                    $comp[$dict[$j]] = $val->find('a')->getAttribute('id');
+                }else{
+                    $comp[$dict[$j]] = $val->innerHtml();
+                }
+                
+                $j++;
+            }
+
+            $company = new App\Company;
+            $company->name = $name;
+            $company->logo = $logo;
+            $company->link = $link;
+            // $company->address = $address;
+
+            if($comp){
+                $company->contact_person = strip_tags($comp['contact_person']);
+                $company->email = base64_decode($comp['email']);
+                $company->phone = base64_decode($comp['phone']);
+                $company->address = strip_tags($comp['alamat']);
+                $company->zipcode = strip_tags($comp['kodepos']);
+                $company->city = strip_tags($comp['kota']);
+                $company->province = strip_tags($comp['provinces']);
+                $company->website = strip_tags($comp['website']);
+            }else{
+                $company->contact_person = '';
+                $company->email = '';
+                $company->phone = '';
+                $company->address = '';
+                $company->zipcode = '';
+                $company->city = '';
+                $company->province = '';
+                $company->website = '';
+            }
+
+            $company->save();
+            }
+
+        }
+
+
+        File::put(storage_path('count7.txt'), $i+1);
+
+    return 'success ' . File::get(storage_path('count7.txt'));
+
+
+});
+
+Route::get('/company8', function () use ($proxies, $proxy) {
+
+    if (isset($proxies)) {
+        $proxy = $proxies[array_rand($proxies)];
+    }
+
+    set_time_limit(1000);   
+
+
+        $i = File::get(storage_path('count8.txt'));
+
+        $i = (int) $i;
+    
+    // for ($i=1; $i<=101537; $i++) {
+    // for ($i=1; $i<=1; $i++) {
+
+        $page = new Dom;
+
+        $page->loadFromUrlProxy('http://m.indonetwork.co.id/search?type=company&page=' . $i, $proxy);
+
+        foreach ($page->find('.list .listingdata') as $list) {
+
+            $link_dom = $list->find('.productdataimg a', 0);
+
+            if ($link_dom) {
+
+            $link = 'http:' . $link_dom->getAttribute('href');
+
+            $thumb = $list->find('.productdataimg a img', 0);
+
+            $logo = 'http://m.indonetwork.co.id' . $thumb->getAttribute('src');
+
+            $name = $list->find('.productdata .content h3 a', 0)->text;
+
+            $address = $list->find('.productdata .companytitle h4', 0)->text;
+
+
+            $company_dom = new Dom;
+            $company_dom->loadFromUrlProxy($link .'/info', $proxy);
+
+            $dict = [
+                'contact_person', 'email', 'phone', 'alamat', 'kodepos', 'negara', 'provinces', 'kota', 'website', 'gabung', 'update'
+            ];
+
+            $j = 0;
+            $comp = [];
+
+            foreach($company_dom->find('.userpage .rightcontent .form .column-9') as $val){
+                if($dict[$j] == 'email' || $dict[$j] == 'phone'){
+                    $comp[$dict[$j]] = $val->find('a')->getAttribute('id');
+                }else{
+                    $comp[$dict[$j]] = $val->innerHtml();
+                }
+                
+                $j++;
+            }
+
+            $company = new App\Company;
+            $company->name = $name;
+            $company->logo = $logo;
+            $company->link = $link;
+            // $company->address = $address;
+
+            if($comp){
+                $company->contact_person = strip_tags($comp['contact_person']);
+                $company->email = base64_decode($comp['email']);
+                $company->phone = base64_decode($comp['phone']);
+                $company->address = strip_tags($comp['alamat']);
+                $company->zipcode = strip_tags($comp['kodepos']);
+                $company->city = strip_tags($comp['kota']);
+                $company->province = strip_tags($comp['provinces']);
+                $company->website = strip_tags($comp['website']);
+            }else{
+                $company->contact_person = '';
+                $company->email = '';
+                $company->phone = '';
+                $company->address = '';
+                $company->zipcode = '';
+                $company->city = '';
+                $company->province = '';
+                $company->website = '';
+            }
+
+            $company->save();
+            }
+
+        }
+
+
+        File::put(storage_path('count8.txt'), $i-1);
+
+    // }
+
+    return 'success ' . File::get(storage_path('count8.txt'));
+
+
+});
+
 Route::get('/product', function() use ($proxies, $proxy) {
 
     $id = File::get(storage_path('category.txt'));
